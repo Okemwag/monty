b9aa80f164dab7ce4bd7b6e1fe14fc1a25b0fa50
Okemwag <gabrielokemwa83@gmail.com>
2022-06-14 04:04:43 -0700

    monty.c

---
+++ opcode_struct.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+
+/**
+* opcode_struct - checks for opcode's associated function and executes it
+* @opcode: opcode from user input
+* @stack: pointer to stack
+* @line_number: line number of opcode from user input
+* Return: 0 on success, 1 on failure
+*/
+int opcode_struct(char *opcode, stack_t **stack, unsigned int line_number)
+{
+	int i;
+
+	instruction_t opcodes[] = {
+		{"pall", pall},
+		{"pint", pint},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"pop", pop},
+		{"swap", swap},
+		{"nop", nop},
+		{"add", _add},
+		{"sub", _sub},
+		{"mul", _mul},
+		{"div", _div},
+		{"mod", _mod},
+		{NULL, NULL}
+	};
+
+	for (i = 0; opcodes[i].opcode != NULL; i++)
+	{
+		if (strcmp(opcode, opcodes[i].opcode) == 0)
+		{
+			(opcodes[i].f)(stack, line_number);
+			return (EXIT_SUCCESS);
+		}
+	}
+	printf("L%d: unknown instruction %s\n", line_number, opcode);
+	exit(EXIT_FAILURE);
+}
